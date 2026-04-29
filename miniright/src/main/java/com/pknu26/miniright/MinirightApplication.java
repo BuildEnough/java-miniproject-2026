784aba9d7f06b787c3fe9e0371dbb1992260648f
BuildEnough <rhtmaehcl878@naver.com>
2026-04-29 14:15:01 +0900

    프로젝트 기본틀 생성

---
+++ miniright/src/main/java/com/pknu26/miniright/MinirightApplication.java
@@ -0,0 +1,13 @@
+package com.pknu26.miniright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinirightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinirightApplication.class, args);
+	}
+
+}
---
+++ miniright/src/test/java/com/pknu26/miniright/MinirightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pknu26.miniright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinirightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
